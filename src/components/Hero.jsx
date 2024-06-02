89c88a074f456a720bc8d617fb4b6251d526c65a
emrecancuhadar <emrecancuhadar@hotmail.com>
2024-06-02 15:38:21 +0300

    motto düzenlemesi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({ onJoinClick }) => {
                 <img src={heroImage} alt="Hero Background" className="w-full h-full object-cover" />
             </div>
             <div className="relative container mx-auto text-center text-white px-4">
-                <h1 className="text-4xl md:text-5xl font-bold">Organized Played Conquered</h1>
+                <h1 className="text-4xl md:text-5xl font-bold">Organized, Played, Conquered</h1>
                 <p className="mt-4 text-lg md:text-xl">World's Biggest Sports Community</p>
                 <div className="mt-8 space-x-4">
                     <button onClick={onJoinClick} className="bg-[#FF5C00] hover:bg-[#e05500] text-white py-2 px-4 rounded">Join Sporzvous</button>
